a2ed6de3d18d5783dbb3a60de66597484a390032
luguobujian <luguobujian@foxmail.com>
2020-03-03 21:36:22 +0800

    first commit

---
+++ src/pages/mine/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <div class="top-info clearfix">
+      <div class="avatar-box fl">
+        <img src=""
+             alt="">
+      </div>
+      <div class="fl">
+        <div>深海淹死一条鱼</div>
+        <div>北京</div>
+        <div></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+  components: {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  height: 100%;
+  background-color: #97d700;
+}
+.top-info {
+  padding: 20px 0 30px;
+  margin: 0 15px;
+}
+.avatar-box img{
+  width: 74px;
+  height: 74px;
+  background-color: aquamarine;
+  border-radius: 50%;
+}
+</style>
